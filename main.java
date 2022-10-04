bdb6187a15f67b7d8df9aa9dec8ac0de46de7766
Wlin29 <wlin@tcd.ie>
2022-10-04 14:35:40 +0100

    created the error check function

--- main.java
+++ main.java
@@ -4,15 +4,15 @@ public class main {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);  // Create a Scanner object
-	    System.out.println("Welcome to Arshad Mohammed and Wenge Lin's Calculator ");
+	    System.out.println("Welcome to Arshad Mohammed and Wen Geng Lin's Calculator ");
 	    System.out.print("Input : ");
 	    String userMathematicalExpression = input.nextLine();
 	    System.out.println(userMathematicalExpression);
-	    
-			
-		
-		
-		
+
+	}
+	int errorCheck(String input){
+		// todo: no special characters
+		// todo: no second operator except if the second character is a minus
 	}
 
 }
