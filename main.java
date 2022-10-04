e6f7a8c2931f5de07bcfab89184a833dbeae9259
Arshad Rehman Mohammed <mohammar@tcd.ie>
2022-10-04 10:03:55 +0100

    Added basic input to main

--- main.java
+++ main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		// 
-		
+		Scanner input = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("Welcome to Arshad Mohammed and Wenge Lin's Calculator ");
+	    System.out.print("Input : ");
+	    String userMathematicalExpression = input.nextLine();
+	    System.out.println(userMathematicalExpression);
+	    
+			
 		
 		
 		
